039053bcc29c7f217e50809d7aedafb44eb9c9a5
fbab <fred@baucher.net>
2016-05-13 19:40:24 +0200

    Update persistanceFacade.java

--- src/main/java/prof/prodageo/org/persistanceFacade.java
+++ src/main/java/prof/prodageo/org/persistanceFacade.java
@@ -1,5 +1,18 @@
 package prof.prodageo.org;
 
+import java.util.Date;
+
 class PersistanceFacade
 {
+  boolean select_unique_user_by_name ( String the_criteria_name , String the_name , Date the_bitrhday )
+  {
+    the_name = "Mickey" ;
+    the_bitrhday = "2010/01/01"
+  }
+  
+  Collections select_users_by_name ( the_selected_name )
+  {
+
+  }
+
 }
