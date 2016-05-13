38d3c9b0bc598555e763940deb821bf1e019a48f
fbab <fred@baucher.net>
2016-05-13 09:40:12 +0200

    refactoring GenMyModel OK

--- src/main/java/prof/prodageo/org/MyUI.java
+++ src/main/java/prof/prodageo/org/MyUI.java
@@ -17,6 +17,10 @@ import com.vaadin.ui.VerticalLayout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/* import for explicit declaration of callback */
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Button.ClickEvent;
+
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
  * (or tab) or some part of a html page where a Vaadin application is embedded.
@@ -30,22 +34,46 @@ public class MyUI extends UI {
 
         private static final Logger log = LoggerFactory.getLogger(MyUIServlet.class);
 
+    /* explicit declaration as attributes of graphical components for GenMyModel */
+        final VerticalLayout layout = new VerticalLayout();
+        final TextField name = new TextField();
+        final TextField surname = new TextField();
+        Button button = new Button("Click Me") ;
+        
+
+    /* explicit callback */
+    /* https://vaadin.com/docs/-/part/framework/application/application-events.html */
+    public class ClickMeClass implements Button.ClickListener
+    {
+        public void buttonClick(ClickEvent event) 
+        {
+            layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
+            log.info("Button clicked with value : " + name.getValue());
+        }
+    }
+        
+
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
 
         
-        final VerticalLayout layout = new VerticalLayout();
+        // final VerticalLayout layout = new VerticalLayout();
         
-        final TextField name = new TextField();
+        // final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
+        /*
         Button button = new Button("Click Me");
         button.addClickListener( e -> {
             layout.addComponent(new Label("Thanks " + name.getValue() 
                     + ", it works!"));
             log.info("Button clicked with value : " + name.getValue());
         });
-        
+        */
+        ClickMeClass callback = new ClickMeClass() ;
+        button.addClickListener( callback ) ;
+
         layout.addComponents(name, button);
         layout.setMargin(true);
         layout.setSpacing(true);
